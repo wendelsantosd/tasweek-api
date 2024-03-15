d5d54b4c3ac6bf75e5c8b585a8d6267193ad4db0
wendelsantosd <wendelwcsantos@gmail.com>
2024-03-15 10:00:02 -0300

    chore: config handling error

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,15 +1,35 @@
 import 'dotenv/config';
-import express, { json } from 'express';
+import 'express-async-errors';
+import express, { json, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import { server } from '@shared/config/env/server';
 import { taskRouter } from '../routes/task.routes';
 import { connectMongoDB } from '../database/mongodb';
+import { AppError } from '@shared/errors/appError';
 
 export const bootstrapAPI = async () => {
 	const app = express();
 	app.use(cors());
 	app.use(json());
 	app.use(taskRouter);
+	app.use(
+		(
+			error: Error,
+			request: Request,
+			response: Response,
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+			next: NextFunction
+		) => {
+			if (error instanceof AppError)
+				return response.status(error.statusCode).json({
+					message: error.message
+				});
+
+			return response.status(500).json({
+				message: `Internal Server Error: ${error.message}`
+			});
+		}
+	);
 
 	connectMongoDB();
 
